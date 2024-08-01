fd4b2488a20e1e9e30dc5e53b460ab1d35310451
Javier Robles <javiroblesfernandez@gmail.com>
2024-08-01 17:48:49 +0200

    Fix level up

--- src/lib/components/level-up/level-up.svelte
+++ src/lib/components/level-up/level-up.svelte
@@ -22,24 +22,35 @@
 	const upgradeKeys = Object.keys(upgrades);
 	const weaponKeys = Object.keys(weapons);
 
-	const selectRandom = () => {
-		const keys = Math.random() < 0.5 ? upgradeKeys : weaponKeys;
-		const index = Math.floor(Math.random() * keys.length);
-		const key = keys[index]!;
-
-		if (keys === weaponKeys) {
-			weaponKeys.splice(index, 1);
-
-			return { key, weapon: true, upgrade: weapons[key as WeaponKey] };
-		} else {
-			upgradeKeys.splice(index, 1);
-
-			return {
-				key,
-				weapon: false,
-				upgrade: upgrades[key as UpgradeKey],
-			};
+	const getKeys = () => {
+		if (weaponKeys.length > 0 && upgradeKeys.length > 0) {
+			return Math.random() < 0.7 ? upgradeKeys : weaponKeys;
 		}
+
+		if (weaponKeys.length > 0) {
+			return weaponKeys;
+		}
+
+		if (upgradeKeys.length > 0) {
+			return upgradeKeys;
+		}
+	};
+
+	const selectRandom = () => {
+		const keys = getKeys();
+
+		if (!keys) {
+			return;
+		}
+
+		const index = Math.floor(Math.random() * keys.length);
+		const key = keys.splice(index, 1)[0]!;
+
+		return {
+			key,
+			weapon: keys === weaponKeys,
+			upgrade: keys === weaponKeys ? weapons[key as WeaponKey] : upgrades[key as UpgradeKey],
+		};
 	};
 
 	const randomUpgrades = [selectRandom(), selectRandom(), selectRandom()];
@@ -53,45 +64,48 @@
 <div class="modal">
 	<Modal open preventCancel>
 		<div class="levelUp">
-			{#each randomUpgrades as { key, weapon, upgrade }}
-				{@const items: Record<string, number | undefined> = weapon ? playerWeapons : playerUpgrades}
-				{@const level = items[key] || 0}
-				<div class="card" class:weapon onclick={handleClick(items, key)} role="none">
-					<div class="level">
-						{#if !level}
-							NEW
-						{:else}
-							Lv.{level}
+			{#each randomUpgrades as randomUpgrade}
+				{#if randomUpgrade}
+					{@const { key, weapon, upgrade } = randomUpgrade}
+					{@const items: Record<string, number | undefined> = weapon ? playerWeapons : playerUpgrades}
+					{@const level = items[key] || 0}
+					<div class="card" class:weapon onclick={handleClick(items, key)} role="none">
+						<div class="level">
+							{#if !level}
+								NEW
+							{:else}
+								Lv.{level}
+							{/if}
+						</div>
+						<div class="title">{upgrade.name}</div>
+
+						{#if "description" in upgrade && !level}
+							<div>{upgrade.description}</div>
 						{/if}
-					</div>
-					<div class="title">{upgrade.name}</div>
 
-					{#if "description" in upgrade && !level}
-						<div>{upgrade.description}</div>
-					{/if}
-
-					<div>
-						{#if "amount" in upgrade}
-							<div class="label">{"label" in upgrade ? upgrade.label : upgrade.name}</div>
-							<div class="difference">
-								<span>{upgrade.format(upgrade.amount(level))}</span>
-								<Icon icon="arrow-right" />
-								<span class="nextValue">{upgrade.format(upgrade.amount(level + 1))}</span>
-							</div>
-						{:else if level}
-							{@const stats = Object.values(upgrade.stats) as (typeof upgrade.stats)[keyof typeof upgrade.stats][]}
-							{#each stats as stat}
-								<div class="label">{stat.label}</div>
-
+						<div>
+							{#if "amount" in upgrade}
+								<div class="label">{"label" in upgrade ? upgrade.label : upgrade.name}</div>
 								<div class="difference">
-									<span>{stat.format(stat.amount(level))}</span>
+									<span>{upgrade.format(upgrade.amount(level))}</span>
 									<Icon icon="arrow-right" />
-									<span class="nextValue">{stat.format(stat.amount(level + 1))}</span>
+									<span class="nextValue">{upgrade.format(upgrade.amount(level + 1))}</span>
 								</div>
-							{/each}
-						{/if}
+							{:else if level}
+								{@const stats = Object.values(upgrade.stats) as (typeof upgrade.stats)[keyof typeof upgrade.stats][]}
+								{#each stats as stat}
+									<div class="label">{stat.label}</div>
+
+									<div class="difference">
+										<span>{stat.format(stat.amount(level))}</span>
+										<Icon icon="arrow-right" />
+										<span class="nextValue">{stat.format(stat.amount(level + 1))}</span>
+									</div>
+								{/each}
+							{/if}
+						</div>
 					</div>
-				</div>
+				{/if}
 			{/each}
 		</div>
 	</Modal>
