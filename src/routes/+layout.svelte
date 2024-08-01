5235dc48461086d561a61a3f2e3b144c118cf638
Javier Robles <javiroblesfernandez@gmail.com>
2024-08-01 23:05:56 +0200

    Refactor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { dev } from "$app/environment";
 	import { Setup } from "$lib/core/components/setup";
 
 	let { children } = $props();
@@ -8,11 +7,3 @@
 <Setup />
 
 {@render children()}
-
-{#if !dev}
-	<script
-		defer
-		src="https://static.cloudflareinsights.com/beacon.min.js"
-		data-cf-beacon={{ token: "977b2509d4e64d1485799479d7dc4204" }}
-	></script>
-{/if}
