12a3afffbf39e30cedd1cc4989c54e1e00d022e2
javi11292 <javi_1_12_92@hotmail.com>
2024-08-04 12:37:45 +0200

    Increase weapon chance

--- src/lib/components/level-up/level-up.svelte
+++ src/lib/components/level-up/level-up.svelte
@@ -5,7 +5,7 @@
 	import { game } from "$lib/state/game";
 	import { player } from "$lib/state/player";
 
-	const UPGRADE_CHANCE = 0.8;
+	const UPGRADE_CHANCE = 0.75;
 	const upgradeKeys = [...player.eligibleUpgrades];
 	const weaponKeys = [...player.eligibleWeapons];
 
